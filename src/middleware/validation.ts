75f1423790815fcc3aa82d1bdb2e4f5191ea3de4
bammy08 <olusolabamidele33@gmail.com>
2024-03-03 20:51:40 +0100

    add image url

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -37,7 +37,7 @@ export const validateMyRestaurantRequest = [
     .withMessage('Delivery price must be a positive number'),
   body('estimatedDeliveryTime')
     .isInt({ min: 0 })
-    .withMessage('Estimated delivery time must be a postivie integar'),
+    .withMessage('Estimated delivery time must be a positive integer'),
   body('cuisines')
     .isArray()
     .withMessage('Cuisines must be an array')
@@ -48,6 +48,6 @@ export const validateMyRestaurantRequest = [
   body('menuItems.*.name').notEmpty().withMessage('Menu item name is required'),
   body('menuItems.*.price')
     .isFloat({ min: 0 })
-    .withMessage('Menu item price is required and must be a postive number'),
+    .withMessage('Menu item price is required and must be a positive number'),
   handleValidationErrors,
 ];
